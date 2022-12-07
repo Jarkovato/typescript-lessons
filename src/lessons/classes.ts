69b08abef85b2ce4270f2d52bfdc322bbe9681d4
Jarkovato <anton.anatolitch@yandex.ru>
2022-12-07 12:31:04 +0300

    feat: composition

--- src/lessons/classes.ts
+++ src/lessons/classes.ts
@@ -38,15 +38,28 @@ class Cars {
 class Animal {
   protected voice: string = "";
   public color: string = "black";
+  private type: string = "general";
 
-  private go(): void {
+  go(): void {
     console.log("go");
   }
 }
 
 class Cat extends Animal {
+  constructor() {
+    super();
+    // can use Animal constructor
+    // and cat constructor
+  }
+
   public setVoice(voice: string): void {
     this.voice = voice;
+  }
+
+  // we can override method
+  override go(): void {
+    // super.go(); // we can use Animal method without override
+    console.log("a cat go");
   }
 }
 
@@ -78,7 +91,11 @@ admin.role = "master";
 
 class Client {
   _login: string;
-  password: string
+  password: string;
+
+  constructor(login: string) {
+    this.login = login;
+  }
 
   get login(): string {
     return this._login;
@@ -88,6 +105,16 @@ class Client {
     this._login = login;
   }
 }
+
+// we can extend Array and use his methods
+class Clients extends Array<Client> {
+  filterByLogin(login: string) {
+    return this.filter((c) => (c.login = login));
+  }
+}
+
+const clients = new Clients();
+clients.push(new Client("client"));
 
 // implement with interface
 interface ILogger {
@@ -109,5 +136,20 @@ class Logger implements ILogger, IDeletable {
 
   delete(): void {
     // delete action
+  }
+}
+
+// composition
+class Payment {
+  date: Date;
+}
+
+class ClientWithPayment {
+  client: Client;
+  payment: Payment;
+
+  constructor(client: Client, payment: Payment) {
+    this.client = client;
+    this.payment = payment;
   }
 }
