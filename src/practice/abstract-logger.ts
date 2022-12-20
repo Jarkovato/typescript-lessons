f9c4855bb2044acd6231375b36ddfc837a5bf56a
Jarkovato <anton.anatolitch@yandex.ru>
2022-12-20 12:53:58 +0300

    feat: practice

---
+++ src/practice/abstract-logger.ts
@@ -0,0 +1,18 @@
+abstract class AbsctractLogger {
+  abstract log(message: string): void;
+
+  printDate() {
+    console.log(new Date());
+  }
+}
+
+class ExtendedLogger extends AbsctractLogger {
+  log(message: string) {
+    console.log(message);
+  }
+
+  logWithDate(): void {
+    this.log('message');
+    this.printDate();
+  }
+}
